3c781f872073cd7167a8088702f695fb7ee48c34
Humayun Mushtaq <69451820+HMUSHTAQ8@users.noreply.github.com>
2022-10-05 18:57:42 +0500

    Add project files.

---
+++ Selenium_Quiz2/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Selenium_Quiz2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
